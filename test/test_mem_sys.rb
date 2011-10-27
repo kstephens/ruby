024f1b5ee67a0728f152986c398711fda799d244
Kurt Stephens <ks.github@kurtstephens.com>
2011-10-27 13:25:24 -0500

    Separate mem_sys event test.

--- test/test_mem_sys.rb
+++ test/test_mem_sys.rb
@@ -1,5 +1,4 @@
 require 'test/unit'
-require 'tempfile'
 require 'pp'
 # require_relative 'ruby/envutil'
 
@@ -21,16 +20,6 @@ class TestMemSys < Test::Unit::TestCase
     $stderr.puts "Running under RUBY_MEM_SYS=#{mem_sys.inspect} MemSys.name=#{MemSys.name.inspect} (supports #{MemSys.supported})"
     # $stderr.puts self.inspect
   end
-
-  def tempfile(*args, &block)
-    t = Tempfile.new(*args, &block)
-    @tempfile = (t unless block)
-  end
-
-  def teardown
-    @tempfile.close! if @tempfile
-  end
-
 
   def test_mem_sys_name
     assert_equal(mem_sys_name, MemSys.name)
@@ -63,48 +52,5 @@ class TestMemSys < Test::Unit::TestCase
     end
   end
 
-  def test_mem_sys_event_log
-    file = tempfile(['mem_sys_event_log', '.txt'])
-    ENV['RUBY_MEM_SYS_EVENT_LOG'] = file.path
-    args = [ argv0, '-e', 'puts (0..10).to_a.inspect' ]
-    pid = Kernel.fork do
-      exec(*args)
-      raise "#{args.inspect} failed"
-    end
-    $stderr.puts "#{args * ' '} pid => #{pid}"
-    Process.waitpid(pid) or raise "failed?"
-
-    o_a = [ ]; o_f = [ ]
-    p_a = [ ]; p_f = [ ]
-    e = [ ]
-
-    contents = File.open(file.to_s) { |fh| fh.readlines }
-    lineno = 0
-    contents.each do | l |
-      lineno += 1
-      l.chomp!
-
-      case l
-      when /^#{pid} (\d+) (\d+) o\{ (0x[0-9a-f]+) (\d+)$/i
-        o_a << l
-      when /^#{pid} (\d+) (\d+) o\} (0x[0-9a-f]+) (\d+)$/i
-        o_f << l
-      when /^#{pid} (\d+) (\d+) p\{ (0x[0-9a-f]+) (\d+)$/i
-        p_a << l
-      when /^#{pid} (\d+) (\d+) p\} (0x[0-9a-f]+) (\d+)$/i
-        p_f << l
-      when /^#{pid} (\d+) (\d+) EXIT$/
-        e << l
-      else
-        raise "Invalid event at event log line #{lineno}: #{l.inspect}"
-      end
-    end # each
-    pp [ o_a.size, o_f.size, p_a.size, p_f.size, e.size ]
-    assert_equal(1, e.size)
-    assert(o_a.size >= o_f.size)
-    assert(p_a.size >= p_f.size)
-    # assert_equal(o_a.size, o_f.size, "object frees == object alloc") # core gc does not abide
-    # assert_equal(p_a.size, p_f.size, "page frees == page alloc")
-  end
 end
 
